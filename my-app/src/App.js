d8d3de1be3221eaafa4d3e31cb44e2da536e0d60
drsuccess <a4surprise@gmail.com>
2020-10-30 11:44:22 +0100

    Complete REACT 1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
 // Must learn function component of REACT
 class App extends Component {
@@ -10,15 +11,39 @@ class App extends Component {
       { name: 'Crystal', age: 25, belt: 'pink', id:3}
     ]
   }
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja]; //push the state obj into new obj as a copy of stateArray
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+  deleteNinja = (id) => {
+    // cycle through the array, return new array
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id
+    });
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+  componentDidMount() {
+    console.log('Componet mounted');
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log('component updated');
+    console.log(prevProps, prevState)
+  }
   render() {
       return (
       <div className="App">
         <h1>My first React App!</h1>
         <p>welcome :)</p>
-        <Ninjas ninjas={this.state.ninjas} />
+        <Ninjas deleteNinja={this.deleteNinja} ninjas={this.state.ninjas} />
+        <AddNinja addNinja={this.addNinja} />
       </div>
     );
-      }
+  }
 }
 
 export default App;
